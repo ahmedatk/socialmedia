68823abebb2df95b89062eac415a3be4d14633fb
Rachit <rachitkum123456@gmail.com>
2023-10-12 11:43:39 +0530

    change

--- main.js
+++ main.js
@@ -1,24 +1,107 @@
 
 
-// function validateForm(event) {
-//     let x = document.forms["myForm"]["username"].value;
-//     if (x != "@") {
-//       alert("email must be valid");
-//       return false;
-//       event.preventDefault();
-//     }
+// const form = document.getElementById('myForm')
+// const username = document.getElementById('username')
+// const password = document.getElementById('password')
+// form.addEventListener('submit', e=>{
+//   e.preventDefault();
+//   validateForm();
+// });
+
+// const setError=(element, message) =>{
+//   const inputcontrol =element.parentElement;
+//   const errorDisplay = inputcontrol.queryselector('.error')
+// }
+
+// const validateForm = () => {
+//   const usernamevalue = username.value.trim()
+//   const passwordvalue = password.value.trim()
+
+//   // let x = document.forms["myForm"]["username"].value;
+//   // if (x != "@") {
+//   //   return false;
+//   //   }
+//   if(passwordvalue=''){
+//     setError(password, 'password should be of 8 chracters')
 //   }
+//   }
+// if(validateForm==false){
+  
+// }
 
-// form.addEventListener('submit',validateForm)
+// const form = document.getElementById('form');
+// const username = document.getElementById('username');
+// const email = document.getElementById('email');
+// const password = document.getElementById('password');
+// const password2 = document.getElementById('password2');
 
-function logSubmit(event) {
-  let x = document.forms["myForm"]["username"].value;
-  if (x != "@") {
-    alert("email must be valid");
-    return false;
-  event.preventDefault();
-}}
+// form.addEventListener('submit', e => {
+//     e.preventDefault();
 
-const form = document.getElementById("myForm");
-const log = document.getElementById("username");
-form.addEventListener("submit", logSubmit);
+//     validateInputs();
+// });
+
+// const setError = (element, message) => {
+//     const inputControl = element.parentElement;
+//     const errorDisplay = inputControl.querySelector('.error');
+
+//     errorDisplay.innerText = message;
+//     inputControl.classList.add('error');
+//     inputControl.classList.remove('success')
+// }
+
+// const setSuccess = element => {
+//     const inputControl = element.parentElement;
+//     const errorDisplay = inputControl.querySelector('.error');
+
+//     errorDisplay.innerText = '';
+//     inputControl.classList.add('success');
+//     inputControl.classList.remove('error');
+// };
+
+// const isValidEmail = email => {
+//     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+//     return re.test(String(email).toLowerCase());
+// }
+
+// const validateInputs = () => {
+//     const usernameValue = username.value.trim();
+//     const emailValue = email.value.trim();
+//     const passwordValue = password.value.trim();
+//     const password2Value = password2.value.trim();
+
+//     if(usernameValue === '') {
+//         setError(username, 'Username is required');
+//     } else {
+//         setSuccess(username);
+//     }
+
+//     if(emailValue === '') {
+//         setError(email, 'Email is required');
+//     } else if (!isValidEmail(emailValue)) {
+//         setError(email, 'Provide a valid email address');
+//     } else {
+//         setSuccess(email);
+//     }
+
+//     if(passwordValue === '') {
+//         setError(password, 'Password is required');
+//     } else if (passwordValue.length < 8 ) {
+//         setError(password, 'Password must be at least 8 character.')
+//     } else {
+//         setSuccess(password);
+//     }
+
+//     if(password2Value === '') {
+//         setError(password2, 'Please confirm your password');
+//     } else if (password2Value !== passwordValue) {
+//         setError(password2, "Passwords doesn't match");
+//     } else {
+//         setSuccess(password2);
+//     }
+
+// };
+
+onclick('submit'){
+  alert('sucess')
+}
