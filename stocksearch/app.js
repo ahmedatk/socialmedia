4572c1e204cc7b7bdb83b678f2a00acbe62a34b0
Rachit <rachitkum123456@gmail.com>
2024-01-07 19:10:34 +0530

    ..

---
+++ stocksearch/app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+
+
+app.get('/fetchData', async (req, res) => {
+    try {
+      const apiKey = 'oeDOoawsGAgTpnK4sbyyzTlCxjW1FFev';
+      const stockSymbol = req.query.stockSymbol || 'AAPL'; // Default to AAPL if no symbol is provided
+  
+      const response = await axios.get(`https://financialmodelingprep.com/api/v3/historical-price-full/${stockSymbol}?apikey=${apiKey}`);
+      const historicalData = response.data.historical;
+  
+      const dates = historicalData.map(item => item.date);
+      const closingPrices = historicalData.map(item => item.close);
+  
+      res.json({ dates, closingPrices });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Error fetching data');
+    }
+  });
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
---
+++ stocksearch/public/script.js
@@ -0,0 +1,50 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Initial load without a specific stock symbol
+    fetchData();
+  });
+  
+  let myChart; // Declare myChart outside of the fetchData function
+  
+  async function fetchData() {
+    try {
+      const stockSymbolInput = document.getElementById('stockSymbol');
+      const stockSymbol = stockSymbolInput.value || 'AAPL'; // Default to AAPL if no symbol is provided
+  
+      const response = await fetch(`/fetchData?stockSymbol=${stockSymbol}`);
+  
+      if (!response.ok) {
+        throw new Error(`Failed to fetch data: ${response.statusText}`);
+      }
+  
+      const contentType = response.headers.get('content-type');
+      if (!contentType || !contentType.includes('application/json')) {
+        throw new Error('Invalid response format');
+      }
+  
+      const data = await response.json();
+  
+      // Destroy the existing chart if it exists
+      if (myChart) {
+        myChart.destroy();
+      }
+  
+      const ctx = document.getElementById('financialChart').getContext('2d');
+      myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+          labels: data.dates,
+          datasets: [{
+            label: 'Closing Prices',
+            data: data.closingPrices,
+            borderColor: 'rgba(75, 192, 192, 1)',
+            borderWidth: 1,
+            fill: false,
+          }]
+        },
+      });
+    } catch (error) {
+      console.error(error);
+      alert('Error fetching data. Please check the stock symbol and try again.');
+    }
+  }
+  
